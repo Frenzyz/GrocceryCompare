ed40c9f5d08c1cdf6e62088d00989c6a660d9a72
Aadit Chetan <AaditC@eastvibestudios.com>
2022-03-26 14:50:27 -0400

    Initial Build of App

---
+++ Shared/IntSlider.swift
@@ -0,0 +1,20 @@
+//
+//  IntSlider.swift
+//  GrocceryCompare
+//
+//  Created by Aadit Chetan on 3/25/22.
+//
+
+import SwiftUI
+
+struct IntSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IntSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        IntSlider()
+    }
+}
---
+++ Shared/Networking/UnitAPI.swift
@@ -0,0 +1,7 @@
+//
+//  UnitAPI.swift
+//  GrocceryCompare
+//
+//  Created by Aadit Chetan on 3/25/22.
+//
+
